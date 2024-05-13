836078b078fbe6f05df6e133a10e7324fb78c102
Ilya Losik <losik998pl@gmail.com>
2024-05-13 23:41:15 +0200

    init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import type { AppProps } from 'next/app';
+
+const theme = createTheme({});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
