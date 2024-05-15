1cdf98ac13e6d6fd048e593df773a6c8cc1ffcf4
Ilya Losik <losik998pl@gmail.com>
2024-05-15 23:13:28 +0200

    add missing key

--- src/components/containers/FeaturesGrid/FeaturesGrid.tsx
+++ src/components/containers/FeaturesGrid/FeaturesGrid.tsx
@@ -3,13 +3,14 @@ import { Grid } from "@mui/material";
 import FeatureCard from "../../presentational/FeatureCard/FeatureCard";
 import { features } from "./constants";
 
-import styles from './FeaturesGrid.module.scss'
+import styles from "./FeaturesGrid.module.scss";
 
 const FeaturesGrid: React.FC = () => {
   return (
     <Grid container className={styles.cardsGrid}>
       {features.map((feature, index) => (
         <FeatureCard
+          key={feature.title}
           title={feature.title}
           description={feature.description}
           icon={feature.icon}
