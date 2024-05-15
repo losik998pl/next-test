cf834dab4a6cb5d2092dd59ec225ede9974fdb2c
Ilya Losik <losik998pl@gmail.com>
2024-05-15 23:00:47 +0200

    style: fix grid, header template for burger and projects section

--- src/components/containers/FeaturesGrid/FeaturesGrid.tsx
+++ src/components/containers/FeaturesGrid/FeaturesGrid.tsx
@@ -3,17 +3,17 @@ import { Grid } from "@mui/material";
 import FeatureCard from "../../presentational/FeatureCard/FeatureCard";
 import { features } from "./constants";
 
+import styles from './FeaturesGrid.module.scss'
+
 const FeaturesGrid: React.FC = () => {
   return (
-    <Grid container spacing={3}>
+    <Grid container className={styles.cardsGrid}>
       {features.map((feature, index) => (
-        <Grid item key={index} xs={12} sm={4} md={3}>
-          <FeatureCard
-            title={feature.title}
-            description={feature.description}
-            icon={feature.icon}
-          />
-        </Grid>
+        <FeatureCard
+          title={feature.title}
+          description={feature.description}
+          icon={feature.icon}
+        />
       ))}
     </Grid>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,12 @@ import Projects from '@/components/containers/Projects/Projects';
 import FeaturesGrid from '@/components/containers/FeaturesGrid/FeaturesGrid';
 
 import styles from './pages.module.scss'
+import Logo from '@/components/shared/Logo/Logo';
 
 const HomePage = () => {
   return (
     <Container className={styles.container}>
+        <Logo mode={'light'} direction='column'/>
         <SliderBanner />
         <Projects />
         <FeaturesGrid />
